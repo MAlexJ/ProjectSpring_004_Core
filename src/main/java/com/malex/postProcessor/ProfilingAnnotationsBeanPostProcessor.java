a1d64ef87408067bf2bc1910599b5ddc3fa5ac7a
malexj <alexmaximovj@gmail.com>
2019-10-16 09:54:46 +0300

    Refactor ProfilingAnnotationsBeanPostProcessor

--- src/main/java/com/malex/postProcessor/ProfilingAnnotationsBeanPostProcessor.java
+++ src/main/java/com/malex/postProcessor/ProfilingAnnotationsBeanPostProcessor.java
@@ -5,10 +5,10 @@ import com.malex.controller.impl.ProfilerController;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
-import javax.management.MBeanServer;
-import javax.management.ObjectName;
+import javax.management.*;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
@@ -24,9 +24,13 @@ public class ProfilingAnnotationsBeanPostProcessor implements BeanPostProcessor 
     /**
      * Issue with MBean see https://stackoverflow.com/questions/25308744/creating-mbean-in-java
      */
-    public ProfilingAnnotationsBeanPostProcessor() throws Exception {
+    public ProfilingAnnotationsBeanPostProcessor()
+            throws MalformedObjectNameException,
+            NotCompliantMBeanException,
+            InstanceAlreadyExistsException,
+            MBeanRegistrationException {
         MBeanServer beanServer = ManagementFactory.getPlatformMBeanServer();
-        ObjectName objectName = new ObjectName("com.malex.controller.impl:type=basic,name=controller");
+        ObjectName objectName = new ObjectName("com.malex.controller.impl:type=ProfilerController,name=controller");
         beanServer.registerMBean(controller, objectName);
     }
 
@@ -43,7 +47,10 @@ public class ProfilingAnnotationsBeanPostProcessor implements BeanPostProcessor 
     public Object postProcessAfterInitialization(Object bean, String beanName) {
         Class beanClass = map.get(beanName);
         if (Objects.nonNull(beanClass)) {
-            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), getInvocationHandler(bean));
+            ClassLoader classLoader = beanClass.getClassLoader();
+            Class[] interfaces = beanClass.getInterfaces();
+            InvocationHandler handler = getInvocationHandler(bean);
+            return Proxy.newProxyInstance(classLoader, interfaces, handler);
         }
         return bean;
     }
@@ -54,10 +61,15 @@ public class ProfilingAnnotationsBeanPostProcessor implements BeanPostProcessor 
                 : method.invoke(bean, args);
     }
 
-    private Object profilingMethod(Object bean, Method method, Object[] args) throws Exception {
-        log.info("Start profiling");
+    private Object profilingMethod(Object bean, Method method, Object[] args)
+            throws InvocationTargetException,
+            IllegalAccessException {
         long before = System.nanoTime();
+        log.info("Start profiling");
+
+        // Invokes the underlying method
         Object retVal = method.invoke(bean, args);
+
         long after = System.nanoTime();
         log.info("End profiling, time: " + (after - before));
         return retVal;
